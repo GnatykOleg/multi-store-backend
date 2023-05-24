503cbb53b0f80fc95c03ab71587fe69810034b1b
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2023-05-24 18:54:44 +0300

    feat: Add products logic
    - Create products.schema.ts
    - Create products.service.ts
    - Create products.controller.ts
    - Create products.module.ts

---
+++ src/modules/products/products.service.ts
@@ -0,0 +1,24 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { Product } from './products.schema';
+import mongoose, { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>,
+  ) {}
+
+  async getCompanyProducts(companyId: string): Promise<Product[]> {
+    try {
+      console.log(mongoose.Types.ObjectId.isValid(companyId));
+      const productForCompany = await this.productModel.find({
+        company: companyId,
+      });
+
+      return productForCompany;
+    } catch (error) {
+      throw new HttpException(error, error.status);
+    }
+  }
+}
